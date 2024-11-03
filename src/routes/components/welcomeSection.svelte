5d3f9cebd366a204b1fc6b8662aedc22202ac779
marlone <marlone.bertuit@epitech.eu>
2024-11-03 14:27:38 +0100

    Update: modified cv download appearance

--- src/routes/components/welcomeSection.svelte
+++ src/routes/components/welcomeSection.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { Badge } from 'flowbite-svelte';
+</script>
+
 <section
 	id="top"
 	class="flex h-screen items-center justify-center bg-gradient-to-r from-gray-900 to-black text-white"
@@ -51,9 +55,12 @@
 				class="text-gray-400 hover:text-white"
 				aria-label="Télécharger CV"
 			>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-			  </svg>
+			<Badge>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+				</svg>
+				<h2 class="ml-2">CV</h2>
+			</Badge>
 			  
 			</a>
 		</div>
